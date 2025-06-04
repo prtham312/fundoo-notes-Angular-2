28ddd5b26e7acfe256326a6deeae3b602fe7054e
prtham312 <pb3488@srmist.edu.in>
2025-06-04 14:24:19 +0530

    dashboard routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+
 ];
