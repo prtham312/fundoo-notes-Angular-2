598904922bdb61d5f1648625424a0908c7427ef2
prtham312 <pb3488@srmist.edu.in>
2025-06-10 17:04:07 +0530

    [Pratham] : Refactor : Added icons and recreated Note-Card

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-note-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule , MatIconModule],
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent {
-  @Input() title: string = '';
-  @Input() description: string = '';
-  @Input() color: string = '';
+  hover = false;
+  isSelected = false;
+
+toggleSelect(event: MouseEvent) {
+  event.stopPropagation();
+  this.isSelected = !this.isSelected;
+}
+@Input() title: string = '';
+@Input() description: string = '';
+@Input() color: string = '';
 @Input() isPinned: boolean = false;
 @Input() isArchived: boolean = false;
 @Input() isDeleted: boolean = false;
